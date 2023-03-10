e0e92d255637117813c48667bb0332b655fda405
kariskan <zkfzpf56@naver.com>
2023-03-10 18:56:24 +0900

    initial commit

---
+++ src/test/java/com/kariskan/spring_batch_practice/SpringBatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kariskan.spring_batch_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
